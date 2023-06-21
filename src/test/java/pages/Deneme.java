d46ff718a6c7314b46052a2a28d140e3063d05cb
gulmek <129228016+Gulmek@users.noreply.github.com>
2023-06-21 23:05:05 +0300

    v/2

--- src/test/java/pages/Deneme.java
+++ src/test/java/pages/Deneme.java
@@ -1,4 +1,8 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class Deneme {
+    public Deneme(){PageFactory.initElements(Driver.getDriver(),this);}
 }
